aa3131a38ebea5e139546fd933077aac66d17ef4
kishorechk <kishorechk@gmail.com>
2023-10-05 13:18:30 +0100

    vpc module

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
     owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -34,7 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
     ami = data.aws_ami.app_ami.id
     instance_type = var.instance_type
-
+    subnet_id              = module.blog_vpc.public_subnets[0]
     vpc_security_group_ids = [module.blog_sg.security_group_id]
 
     tags = {
@@ -47,18 +47,11 @@ module "blog_sg" {
     version = "5.1.0"
 
     name    = "blog_new"
-    vpc_id  = module.vpc.public_subnets[0]
+    vpc_id  = module.blog_vpc.public_subnets[0]
 
     ingress_rules       = ["http-80-tcp", "https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
 
     egress_rules        = ["all-all"]
     egress_cidr_blocks  = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "blog" {
-    name = "blog"
-    description = "Allow http and https in. Allow everything out"
-
-    vpc_id = data.aws_vpc.default.id
 }
