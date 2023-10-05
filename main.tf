261b1e7714d9245dc09b2cc9208d04330e6d1b60
kishorechk <kishorechk@gmail.com>
2023-10-05 13:22:09 +0100

    vpc module

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ module "blog_vpc" {
 }
 
 resource "aws_instance" "blog" {
-    ami = data.aws_ami.app_ami.id
-    instance_type = var.instance_type
-    subnet_id              = module.blog_vpc.public_subnets[0]
-    vpc_security_group_ids = [module.blog_sg.security_group_id]
+    ami                     = data.aws_ami.app_ami.id
+    instance_type           = var.instance_type
+    subnet_id               = module.blog_vpc.public_subnets[0]
+    vpc_security_group_ids  = [module.blog_sg.security_group_id]
 
     tags = {
         Name = "Learning Terraform"
@@ -46,8 +46,8 @@ module "blog_sg" {
     source  = "terraform-aws-modules/security-group/aws"
     version = "5.1.0"
 
-    name    = "blog_new"
-    vpc_id  = module.blog_vpc.public_subnets[0]
+    name    = "blog"
+    vpc_id  = module.blog_vpc.vpc_id
 
     ingress_rules       = ["http-80-tcp", "https-443-tcp"]
     ingress_cidr_blocks = ["0.0.0.0/0"]
